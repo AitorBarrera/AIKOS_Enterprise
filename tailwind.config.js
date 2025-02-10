13429c63b49f77ca068788bf93e17274593eb105
NoNiQu <marcos.workstation@gmail.com>
2025-02-10 13:39:05 +0100

    cambios sin sentido

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,45 @@
 module.exports = {
-    theme: {
-        extend: {
-            colors: {
-                'p-ruby': '#e0115f',
-                'p-ruby-disable': '#7d0a36',
-                'p-ruby-hover': '#b00e4c',
-                's-yellow': '#ffc700',
-                's-yellow-old': '#a67c52',
-                'black': '#000000',
-                'grey': '#c4c4c4',
-                'white': '#ffffff',
-            },
-            icon: {
-                'icon-collab-rrss': '2.5rem',
-                'icon-other': '3.125rem',
-                'icon-logo': '3.75rem',
-            },
-            sizes: {
-                'lineheight-light': '1.5rem',
-                'lineheight-regular': '1.875rem',
-                'lineheight-h2': '3rem',
-                'lineheight-h3': '3rem',
-                'lineheight-h1': '4.5rem',
-                'size-light': '1rem',
-                'size-regular': '1.25rem',
-                'size-h3': '1.625rem',
-                'size-h2': '2.625rem',
-                'size-h1': '4.25rem',
-            },
-            weight: {
-                'weight-bold': 'bold',
-                'weight-semibold': 'semibold',
-                'weight-light': 'light',
-                'weight-medium': 'medium',
-                'weight-regular': 'regular',
-            },
-            fontFamily: {
-                'family-outfit': 'Outfit',
-            },
-        },
+  theme: {
+    extend: {
+      colors: {
+        "p-ruby": "#e0115f",
+        "p-ruby-disable": "#7d0a36",
+        "p-ruby-hover": "#b00e4c",
+        "s-yellow": "#ffc700",
+        "s-yellow-old": "#a67c52",
+        black: "#000000",
+        grey: "#c4c4c4",
+        white: "#ffffff",
+      },
+      spacing: {
+        "icon-collab-rrss": "2.5rem",
+        "icon-other": "3.125rem",
+        "icon-logo": "3.75rem",
+      },
+      lineHeight: {
+        light: "1.5rem",
+        regular: "1.875rem",
+        h2: "3rem",
+        h3: "3rem",
+        h1: "4.5rem",
+      },
+      fontSize: {
+        light: "1rem",
+        regular: "1.25rem",
+        h3: "1.625rem",
+        h2: "2.625rem",
+        h1: "4.25rem",
+      },
+      fontWeight: {
+        bold: "bold",
+        semibold: "semibold",
+        light: "light",
+        medium: "medium",
+        regular: "regular",
+      },
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+      },
     },
+  },
 };
