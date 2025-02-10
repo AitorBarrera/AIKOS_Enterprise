816e042a2e78c82d22410b78cd2d78a051d34c30
Barrera <alumno.138627@ies-azarquiel.es>
2025-02-10 13:07:29 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+    theme: {
+        extend: {
+            color: {
+                'p-ruby': '#e0115f',
+                'p-ruby-disable': '#7d0a36',
+                'p-ruby-hover': '#b00e4c',
+                's-yellow': '#ffc700',
+                's-yellow-old': '#a67c52',
+                'black': '#000000',
+                'grey': '#c4c4c4',
+                'white': '#ffffff',
+            },
+            icon: {
+                'icon-collab-rrss': '2.5rem',
+                'icon-other': '3.125rem',
+                'icon-logo': '3.75rem',
+            },
+            sizes: {
+                'lineheight-light': '1.5rem',
+                'lineheight-regular': '1.875rem',
+                'lineheight-h2': '3rem',
+                'lineheight-h3': '3rem',
+                'lineheight-h1': '4.5rem',
+                'size-light': '1rem',
+                'size-regular': '1.25rem',
+                'size-h3': '1.625rem',
+                'size-h2': '2.625rem',
+                'size-h1': '4.25rem',
+            },
+            weight: {
+                'weight-bold': 'bold',
+                'weight-semibold': 'semibold',
+                'weight-light': 'light',
+                'weight-medium': 'medium',
+                'weight-regular': 'regular',
+            },
+            fontFamily: {
+                'family-outfit': 'Outfit',
+            },
+        },
+    },
+};
