63a3d825f98572a490163d38fd2c3772b3d909ae
NoNiQu <marcos.workstation@gmail.com>
2025-02-18 09:37:32 +0100

    commit para fixed

--- app/componentes/CardContacto.tsx
+++ app/componentes/CardContacto.tsx
@@ -7,7 +7,7 @@ interface CardContactoProps {
 
 function CardContacto({ icono, texto }: CardContactoProps) {
   return (
-    <div className="flex flex-col items-center gap-4 px-4 py-4 text-black bg-white border-4">
+    <div className="flex flex-col items-center gap-4 p-4 text-black bg-white border-4">
       <img src={icono} alt="" className="w-[12.5%]" />
       <p className="text-3xl">{texto}</p>
     </div>
